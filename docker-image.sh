8330666a81eae12af6a98b54b4ea04553feb6c47
FawziLinggo <fawzilinggo@gmail.com>
2022-09-01 15:44:56 +0700

    from and run instruction

--- docker-image.sh
+++ docker-image.sh
@@ -1,1 +1,12 @@
+# see image
 docker image ls
+
+# pull {redis}
+docker image pull redis:latest
+
+# Remove Docker
+# docker rm image nameimage:latest
+docker rm image redis:latest
+
+# See Container
+docker container ls -a
---
+++ script.sh
@@ -0,0 +1,5 @@
+# FROM instruction
+docker build -t fawzilinggo/file file
+
+# RUN instruction
+docker build -t fawzilinggo/run run
